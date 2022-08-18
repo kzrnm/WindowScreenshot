c51a17b9d9020327a0cebc4c4f130341396ad849
kzrnm <gengesa@gmail.com>
2022-08-18 17:14:11 +0900

    initial

---
+++ Tests/WindowScreenshot.Shared.Test/Mvvm/SelectorObservableCollectionTest.cs
@@ -0,0 +1,219 @@
+﻿using KzLibraries.EventHandlerHistory;
+
+namespace WindowScreenshot;
+
+using SelectedItemChangedEventArgs = SelectorObservableCollection<string>.SelectedItemChangedEventArgs;
+public class SelectorObservableCollectionTests
+{
+    readonly SelectorObservableCollection<string> collection;
+    readonly List<SelectedItemChangedEventArgs> selectedChangedHistory;
+    readonly PropertyChangedHistory properyChangedHistory;
+    public SelectorObservableCollectionTests()
+    {
+        collection = new();
+        selectedChangedHistory = new();
+        properyChangedHistory = new(collection);
+        collection.SelectedChanged += (_, e) => selectedChangedHistory.Add(e);
+    }
+
+    [Fact]
+    public void AddAndInsert()
+    {
+        selectedChangedHistory.Should().BeEmpty();
+        collection.SelectedIndex.Should().Be(-1);
+        collection.SelectedItem.Should().Be(null);
+        collection.Should().BeEmpty();
+
+        collection.Add("foo");
+        collection.Should().Equal("foo");
+        selectedChangedHistory.Should().ContainSingle().Which.Should().BeEquivalentTo(new SelectedItemChangedEventArgs(-1, 0, null, "foo"));
+        collection.SelectedIndex.Should().Be(0);
+        collection.SelectedItem.Should().Be("foo");
+
+        collection.Add("bar");
+        collection.Should().Equal("foo", "bar");
+        selectedChangedHistory.Should().BeEquivalentTo(
+            new[]{
+            new SelectedItemChangedEventArgs(-1, 0, null, "foo"),
+            new SelectedItemChangedEventArgs(0, 1, "foo", "bar"),
+            });
+        collection.SelectedIndex.Should().Be(1);
+        collection.SelectedItem.Should().Be("bar");
+
+        collection.Insert(1, "baz");
+        collection.Should().Equal("foo", "baz", "bar");
+        selectedChangedHistory.Should().BeEquivalentTo(
+            new[]{
+            new SelectedItemChangedEventArgs(-1, 0, null, "foo"),
+            new SelectedItemChangedEventArgs(0, 1, "foo", "bar"),
+            new SelectedItemChangedEventArgs(1, 1, "bar", "baz"),
+        });
+        collection.SelectedIndex.Should().Be(1);
+        collection.SelectedItem.Should().Be("baz");
+    }
+
+    [Fact]
+    public void Remove()
+    {
+        collection.Add("foo");
+        collection.Add("bar");
+        collection.Add("baz");
+        collection.Add("fizz");
+        collection.Add("buzz");
+        collection.SelectedIndex = -1;
+        selectedChangedHistory.Clear();
+        properyChangedHistory.Clear();
+
+        collection.SelectedIndex.Should().Be(-1);
+        collection.SelectedItem.Should().Be(null);
+        collection.Should().Equal("foo", "bar", "baz", "fizz", "buzz");
+        selectedChangedHistory.Should().BeEmpty();
+
+        collection.RemoveSelectedItem();
+        collection.SelectedIndex.Should().Be(-1);
+        collection.SelectedItem.Should().Be(null);
+        collection.Should().Equal("foo", "bar", "baz", "fizz", "buzz");
+        selectedChangedHistory.Should().BeEmpty();
+
+        collection.Remove("fizz");
+        collection.SelectedIndex.Should().Be(-1);
+        collection.SelectedItem.Should().Be(null);
+        collection.Should().Equal("foo", "bar", "baz", "buzz");
+        selectedChangedHistory.Should().BeEmpty();
+
+        collection.SelectedIndex = 3;
+        selectedChangedHistory.Clear();
+        properyChangedHistory.Clear();
+        collection.Remove("buzz");
+        collection.SelectedIndex.Should().Be(2);
+        collection.SelectedItem.Should().Be("baz");
+        collection.Should().Equal("foo", "bar", "baz");
+        
+        selectedChangedHistory.Should().ContainSingle().Which.Should().BeEquivalentTo(new SelectedItemChangedEventArgs(3, 2, "buzz", "baz"));
+
+        collection.SelectedIndex = 1;
+        selectedChangedHistory.Clear();
+        properyChangedHistory.Clear();
+        collection.RemoveAt(0);
+        collection.SelectedIndex.Should().Be(0);
+        collection.SelectedItem.Should().Be("bar");
+        collection.Should().Equal("bar", "baz");
+        
+        selectedChangedHistory.Should().ContainSingle().Which.Should().BeEquivalentTo(new SelectedItemChangedEventArgs(1, 0, "bar", "bar"));
+
+        selectedChangedHistory.Clear();
+        properyChangedHistory.Clear();
+        collection.RemoveAt(0);
+        collection.SelectedIndex.Should().Be(0);
+        collection.SelectedItem.Should().Be("baz");
+        collection.Should().Equal("baz");
+        
+        selectedChangedHistory.Should().ContainSingle().Which.Should().BeEquivalentTo(new SelectedItemChangedEventArgs(0, 0, "bar", "baz"));
+
+        selectedChangedHistory.Clear();
+        properyChangedHistory.Clear();
+        collection.RemoveSelectedItem();
+        collection.SelectedIndex.Should().Be(-1);
+        collection.SelectedItem.Should().Be(null);
+        collection.Should().BeEmpty();
+        
+        selectedChangedHistory.Should().ContainSingle().Which.Should().BeEquivalentTo(new SelectedItemChangedEventArgs(0, -1, "baz", null));
+    }
+
+    [Fact]
+    public void Clear()
+    {
+        collection.Add("foo");
+        collection.Add("bar");
+        collection.Add("baz");
+        collection.Add("fizz");
+        collection.Add("buzz");
+        collection.Should().Equal("foo", "bar", "baz", "fizz", "buzz");
+        collection.SelectedIndex = -1;
+        selectedChangedHistory.Clear();
+        properyChangedHistory.Clear();
+
+        collection.Clear();
+        collection.SelectedIndex.Should().Be(-1);
+        collection.SelectedItem.Should().Be(null);
+        collection.Should().BeEmpty();
+        selectedChangedHistory.Should().BeEmpty();
+
+
+        collection.Add("foo");
+        collection.Add("bar");
+        collection.Add("baz");
+        collection.Add("fizz");
+        collection.Add("buzz");
+        collection.Should().Equal("foo", "bar", "baz", "fizz", "buzz");
+        collection.SelectedIndex = 0;
+        selectedChangedHistory.Clear();
+        properyChangedHistory.Clear();
+
+        collection.Clear();
+        collection.SelectedIndex.Should().Be(-1);
+        collection.SelectedItem.Should().Be(null);
+        collection.Should().BeEmpty();
+        
+        selectedChangedHistory.Should().ContainSingle().Which.Should().BeEquivalentTo(new SelectedItemChangedEventArgs(0, -1, "foo", null));
+    }
+
+    [Fact]
+    public void Move()
+    {
+        collection.Add("foo");
+        collection.Add("bar");
+        collection.Add("baz");
+        collection.Add("fizz");
+        collection.Add("buzz");
+        collection.Should().Equal("foo", "bar", "baz", "fizz", "buzz");
+        collection.SelectedIndex = -1;
+        selectedChangedHistory.Clear();
+        properyChangedHistory.Clear();
+
+        collection.Move(1, 3);
+        collection.SelectedIndex.Should().Be(-1);
+        collection.SelectedItem.Should().Be(null);
+        collection.Should().Equal("foo", "baz", "fizz", "bar", "buzz");
+        selectedChangedHistory.Should().BeEmpty();
+
+        collection.SelectedIndex = 2;
+        collection.SelectedIndex.Should().Be(2);
+        collection.SelectedItem.Should().Be("fizz");
+        selectedChangedHistory.Clear();
+        properyChangedHistory.Clear();
+        collection.Move(2, 3);
+        collection.SelectedIndex.Should().Be(3);
+        collection.SelectedItem.Should().Be("fizz");
+        collection.Should().Equal("foo", "baz", "bar", "fizz", "buzz");
+        
+        selectedChangedHistory.Should().ContainSingle().Which.Should().BeEquivalentTo(new SelectedItemChangedEventArgs(2, 3, "fizz", "fizz"));
+    }
+
+
+    [Fact]
+    public void SetItem()
+    {
+        collection.Add("foo");
+        collection.Add("bar");
+        collection.Add("baz");
+        collection.Add("fizz");
+        collection.Add("buzz");
+        collection.Should().Equal("foo", "bar", "baz", "fizz", "buzz");
+        collection.SelectedIndex = -1;
+        selectedChangedHistory.Clear();
+        properyChangedHistory.Clear();
+
+        collection[0] = "fx";
+        collection.Should().Equal("fx", "bar", "baz", "fizz", "buzz");
+        selectedChangedHistory.Should().BeEmpty();
+
+        collection.SelectedIndex = 2;
+        selectedChangedHistory.Clear();
+        properyChangedHistory.Clear();
+        collection[2] = "bx";
+        collection.Should().Equal("fx", "bar", "bx", "fizz", "buzz");
+
+        selectedChangedHistory.Should().ContainSingle().Which.Should().BeEquivalentTo(new SelectedItemChangedEventArgs(2, 2, "baz", "bx"));
+    }
+}
---
+++ WindowScreenshot.Shared/Mvvm/SelectorObservableCollection.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace WindowScreenshot;
+public class SelectorObservableCollection<T> : ObservableCollection<T>
+{
+    private int _SelectedIndex = -1;
+    public int SelectedIndex
+    {
+        set
+        {
+            if (_SelectedIndex != value)
+            {
+                var prevIndex = _SelectedIndex;
+                _SelectedIndex = value;
+                OnSelectedIndexChanged(prevIndex, value);
+            }
+        }
+        get => _SelectedIndex;
+    }
+    public T? SelectedItem => GetOrDefault(SelectedIndex);
+    private T? GetOrDefault(int index) => (uint)index < (uint)Count ? this[index] : default;
+
+    public event EventHandler<SelectedItemChangedEventArgs>? SelectedChanged;
+    private void OnSelectedItemChanged(T? oldItem, T? newItem)
+    {
+        SelectedChanged?.Invoke(this, new(SelectedIndex, SelectedIndex, oldItem, newItem));
+        OnPropertyChanged(new(nameof(SelectedItem)));
+    }
+    private void OnSelectedIndexChanged(int oldIndex, int newIndex)
+    {
+        var oldItem = GetOrDefault(oldIndex);
+        var newItem = GetOrDefault(newIndex);
+        SelectedChanged?.Invoke(this, new(oldIndex, newIndex, oldItem, newItem));
+        OnPropertyChanged(new(nameof(SelectedIndex)));
+    }
+    private void OnSelectedChanged(int oldIndex, int newIndex, T? oldItem, T? newItem)
+    {
+        SelectedChanged?.Invoke(this, new(oldIndex, newIndex, oldItem, newItem));
+        OnPropertyChanged(new(nameof(SelectedIndex)));
+        OnPropertyChanged(new(nameof(SelectedItem)));
+    }
+    protected override void InsertItem(int index, T item)
+    {
+        var oldIndex = SelectedIndex;
+        var oldItem = SelectedItem;
+        base.InsertItem(index, item);
+        _SelectedIndex = index;
+        OnSelectedChanged(oldIndex, index, oldItem, item);
+    }
+
+    protected override void ClearItems()
+    {
+        var oldIndex = SelectedIndex;
+        var oldItem = SelectedItem;
+        base.ClearItems();
+        if (_SelectedIndex != -1)
+            OnSelectedChanged(oldIndex, _SelectedIndex = -1, oldItem, default);
+    }
+
+    protected override void MoveItem(int oldIndex, int newIndex)
+    {
+        base.MoveItem(oldIndex, newIndex);
+        if (oldIndex == SelectedIndex)
+        {
+            _SelectedIndex = newIndex;
+            var item = SelectedItem;
+            OnPropertyChanged(new(nameof(SelectedIndex)));
+            SelectedChanged?.Invoke(this, new(oldIndex, newIndex, item, item));
+        }
+    }
+    protected override void RemoveItem(int index)
+    {
+        var prevSelected = SelectedIndex;
+        var prevSelectedItem = SelectedItem;
+        base.RemoveItem(index);
+        if (Count == 0)
+        {
+            _SelectedIndex = -1;
+            OnSelectedChanged(prevSelected, -1, prevSelectedItem, SelectedItem);
+        }
+        else if (index == prevSelected)
+        {
+            _SelectedIndex = Math.Min(prevSelected, Count - 1);
+            OnSelectedChanged(prevSelected, SelectedIndex, prevSelectedItem, SelectedItem);
+        }
+        else if (index < prevSelected)
+        {
+            _SelectedIndex = prevSelected - 1;
+            OnSelectedChanged(prevSelected, SelectedIndex, prevSelectedItem, SelectedItem);
+        }
+    }
+    protected override void SetItem(int index, T item)
+    {
+        if (index != SelectedIndex)
+        {
+            base.SetItem(index, item);
+            return;
+        }
+        var prevSelectedItem = SelectedItem;
+        base.SetItem(index, item);
+        OnSelectedItemChanged(prevSelectedItem, item);
+    }
+
+    public void RemoveSelectedItem()
+    {
+        var ix = SelectedIndex;
+        if ((uint)ix < (uint)Count)
+            this.RemoveAt(ix);
+    }
+
+
+    public class SelectedItemChangedEventArgs : EventArgs
+    {
+        public int OldIndex { get; }
+        public int NewIndex { get; }
+        public T? OldItem { get; }
+        public T? NewItem { get; }
+        public SelectedItemChangedEventArgs(int oldIndex, int newIndex, T? oldItem, T? newItem)
+        {
+            OldIndex = oldIndex;
+            NewIndex = newIndex;
+            OldItem = oldItem;
+            NewItem = newItem;
+        }
+    }
+}
